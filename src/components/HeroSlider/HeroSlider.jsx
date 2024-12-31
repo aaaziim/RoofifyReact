af1b001d83e462347fc1eb7e0ce0693f28e4fb0f
aaaziim <azim210215@gmail.com>
2024-12-31 10:53:00 +0600

    Hero Slider Updated

--- src/components/HeroSlider/HeroSlider.jsx
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -5,15 +5,16 @@ const HeroSlider = () => {
       <div className="carousel min-h-[500px] w-full rounded-2xl my-6 ">
   <div id="slide1" className="carousel-item relative w-full">
    
-      <div 
-      className="hero bg-[url('https://cdn.pixabay.com/photo/2013/04/07/01/10/roof-101372_1280.jpg')] bg-cover bg-center">
-  <div className="hero-content flex-col lg:flex-row px-6">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News 1</h1>
-      <p className="py-6">
+  <div
+  className="hero m "
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
@@ -27,14 +28,16 @@ const HeroSlider = () => {
     </div>
   </div>
   <div id="slide2" className="carousel-item relative w-full">
-  <div className="hero bg-gradient-to-r from-indigo-500  ">
-  <div className="hero-content flex-col lg:flex-row px-6">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News 2</h1>
-      <p className="py-6">
+  <div
+  className="hero m "
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
@@ -48,14 +51,16 @@ const HeroSlider = () => {
     </div>
   </div>
   <div id="slide3" className="carousel-item relative w-full">
-  <div className="hero bg-gradient-to-r bg-gradient-to-r from-indigo-400 to-green-600 ">
-  <div className="hero-content flex-col lg:flex-row px-6">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News 3</h1>
-      <p className="py-6">
+  <div
+  className="hero m "
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
@@ -69,14 +74,16 @@ const HeroSlider = () => {
     </div>
   </div>
   <div id="slide4" className="carousel-item relative w-full">
-  <div className="hero bg-gradient-to-r from-indigo-500  ">
-  <div className="hero-content flex-col lg:flex-row px-6">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">Box Office News</h1>
-      <p className="py-6">
+  <div
+  className="hero m "
+  style={{
+    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+      <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
       </p>
