c975cb06229bc6697c4581c1eaaa0e15bbfacb0f
aaaziim <azim210215@gmail.com>
2024-12-31 14:51:53 +0600

    Services Grid Added

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,78 @@
 import HeroSlider from "../../HeroSlider/HeroSlider";
-
+import { FaHouse } from "react-icons/fa6";
 const Home = () => {
     return (
         <div>
             <HeroSlider></HeroSlider>
+            <div className="p-4 bg-blue-900 py-8    rounded-xl ">
+            <h1 className="text-2xl font-bold text-center text-white">
+                   Our Services
+                    </h1>
+                    </div>
+            <div className="grid grid-cols-1  md:grid-cols-4 my-4 gap-4 p-4 bg-blue-900 py-8  m  rounded-xl ">
+            <div className="flex flex-col items-center bg-gray-200 text-black space-y-3 p-4 rounded-xl border border-yellow-100 hover:scale-105 hover:border-yellow-200 hover:border-2">
+                <FaHouse className="text-4xl" />
+                <p className="text-center font-semibold py-3">
+                RoofifyReact provides a reliable, high-quality, and affordable commercial roof repair service in Charleston and the surrounding region.
+                </p>
+                <a href="#_" className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500   hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+    <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+    <span class="relative">Roof Repair</span>
+                </a>
+                </div> <div className="flex flex-col items-center bg-gray-200 text-black space-y-3 p-4 rounded-xl border border-yellow-100 hover:scale-105 hover:border-yellow-200 hover:border-2">
+                <FaHouse className="text-4xl" />
+                <p className="text-center font-semibold py-3">
+                RoofifyReact provides a reliable, high-quality, and affordable commercial roof repair service in Charleston and the surrounding region.
+                </p>
+                <a href="#_" className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500   hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+    <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+    <span class="relative">Roof Repair</span>
+                </a>
+                </div> <div className="flex flex-col items-center bg-gray-200 text-black space-y-3 p-4 rounded-xl border border-yellow-100 hover:scale-105 hover:border-yellow-200 hover:border-2">
+                <FaHouse className="text-4xl" />
+                <p className="text-center font-semibold py-3">
+                RoofifyReact provides a reliable, high-quality, and affordable commercial roof repair service in Charleston and the surrounding region.
+                </p>
+                <a href="#_" className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500   hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+    <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+    <span class="relative">Roof Repair</span>
+                </a>
+                </div> <div className="flex flex-col items-center bg-gray-200 text-black space-y-3 p-4 rounded-xl border border-yellow-100 hover:scale-105 hover:border-yellow-200 hover:border-2">
+                <FaHouse className="text-4xl" />
+                <p className="text-center font-semibold py-3">
+                RoofifyReact provides a reliable, high-quality, and affordable commercial roof repair service in Charleston and the surrounding region.
+                </p>
+                <a href="#_" className="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500   hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300">
+    <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+    <span class="relative">Roof Repair</span>
+                </a>
+                </div>
+            </div>
+
+
+            <div className="bg-blue-900 py-8  my-6 rounded-xl md:px-44">
+                    <h1 className="text-2xl font-bold text-center text-white">
+                    What Makes a Good Roofing Company?
+                    </h1>
+                    <p className="text-center my-4 text-white">
+            In this resource, we’ll discuss the qualities that make a good roofing company, as well as what to keep in mind when picking a roofing company.
+Discover what sets Roofing USA apart as a top roofing company in Charleston.
+            </p>
+            <p className="text-center p-4">
+            <a href="#_" class="relative inline-block text-lg group">
+    <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+        <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+        <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+        <span class="relative">Find Out Here</span>
+    </span>
+    <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+</a>
+            </p>
+            </div>
+
+
+
+
         </div>
     );
 };
